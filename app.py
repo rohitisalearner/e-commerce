323d45adb5bd219f2a7960f4b39dcc8e4b611870
shahebjan <shahabjanahamad0786@gmail.com>
2024-02-19 14:14:05 +0530

    ok

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from models import db
 app=Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://root:rohit123@localhost/'
+app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://root@localhost/ecommerce'
 app.config['SQLALCHEMY_POOL_RECYCLE'] = 3600
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
--- models.py
+++ models.py
@@ -3,8 +3,12 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class topWear(db.Model):
-    __tablename__ = "topWear"
+    __tablename__ = 'topWear'
 
-    id = db.column(db.Integer, primary_key = True)
+    id = db.column(db.Integer, primary_key=True)
     categoryName = db.column(db.String(100), unique = True)
-    categoryName = db.column(db.String(100), unique = True)
+    categoryTitle = db.column(db.String(200))
+    categoryDescription = db.column(db.String(1000))
+    categoryImage = db.column(db.LargeBinary(length=16777215))
+    categoryListPrice = db.column(db.Integer)
+    categoryMrpPrice = db.column(db.Integer)
