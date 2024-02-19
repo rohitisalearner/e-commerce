d8efa39eb61a35d7bafcddaac6d4f15bbe58960f
Rohit Sonkar <airohitit@gmail.com>
2024-02-19 14:09:26 +0530

    app file added

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from models import db
+app=Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://root:rohit123@localhost/'
+app.config['SQLALCHEMY_POOL_RECYCLE'] = 3600
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+
+@app.route('/')
+def home():
+    return "added"
+
+if __name__=="__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
